5d47ba32f14d269ab04ed08ae854d7ebd55ba986
Aᴡɴɪ <113506682+TahaNabavi@users.noreply.github.com>
2024-09-08 14:07:32 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const mapLink = "http://5.42.223.21/map/Map.jpg"
+export const mapLink = "https://s32.picofile.com/file/8478873250/Map_1_.jpg`"
 export const apiLink = "http://5.42.223.21:2569"
 export const apiKey = "18839F42DB184DD062A719F2856CBD09"
