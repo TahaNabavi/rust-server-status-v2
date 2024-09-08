8a64456615ec95a4a68a33d9ba93887ea06d21c6
Aᴡɴɪ <113506682+TahaNabavi@users.noreply.github.com>
2024-09-08 13:45:18 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ function PlayerCard({ data }: { data: Player }) {
             href={`https://steamcommunity.com/profiles/${data.SteamID}`}
             target="_blank"
             rel="noreferrer"
-            className="text-2xl text-white me-2"
+            className="text-2xl text-white me-2 text-hidden"
           >
             {data.Name}
           </a>
