533e822218bfdcc2209ca6823bd328845f37693e
Aᴡɴɪ <113506682+TahaNabavi@users.noreply.github.com>
2024-09-27 10:56:44 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const mapLink = "https://s32.picofile.com/file/8478873250/Map_1_.jpg`"
-export const apiLink = "http://5.42.223.21:2569"
+export const apiLink = "http://5.42.223.21:2569/?pass=MyNigger"
 export const apiKey = "18839F42DB184DD062A719F2856CBD09"
